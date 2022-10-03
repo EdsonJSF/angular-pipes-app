f7f951f18c5c7d98e201ddeab49c69b55e635df6
Edsonjsf <edsonjsf1990@gmail.com>
2022-10-03 14:25:18 -0400

    Instalacion de PrimeNg y uso con ejemplos de algunos pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pipes-app';
+  nombre: string = 'pipes-app';
+  valor: number = 1000;
+  objeto = {
+    valor: "objeto"
+  };
+
+  cambiarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.objeto);
+  }
 }
