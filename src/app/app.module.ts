14467380ae170e277eafdefb5732b3d61d8084d8
Edsonjsf <edsonjsf1990@gmail.com>
2022-10-10 13:58:46 -0400

    Se agrego y configuro el Fieldset de PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
@@ -8,16 +9,22 @@ import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
 // Cambiar el locale de la app
-import localesEs from "@angular/common/locales/es";
-import localesFr from "@angular/common/locales/fr";
-import { registerLocaleData } from "@angular/common";
-registerLocaleData(localesEs)
-registerLocaleData(localesFr)
+import localesEs from '@angular/common/locales/es';
+import localesFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localesEs);
+registerLocaleData(localesFr);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
-  providers: [{provide: LOCALE_ID, useValue: 'es'}],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule,
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/modules/prime-ng/prime-ng.module.ts
+++ src/app/modules/prime-ng/prime-ng.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import {ButtonModule} from 'primeng/button';
-import {CardModule} from 'primeng/card';
-import {MenubarModule} from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
-  exports: [
-    ButtonModule,
-    CardModule,
-    MenubarModule
-  ]
+  exports: [ButtonModule, CardModule, FieldsetModule, MenubarModule],
 })
-export class PrimeNgModule { }
+export class PrimeNgModule {}
